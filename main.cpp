923c27c165f1a70523ebca4cf5b05a9d12b2fc33
jt_gladden <jt_gladden@aol.com>
2024-11-22 14:07:27 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  const char dog[] = "      / \\__\n"
+                     "     (    @\\___\n"
+                     "     /         O\n"
+                     "    /   (_____/\n"
+                     "   /_____/   U\n"
+                     "  /           \\\n"
+                     " /             \\\n"
+                     "|               |\n"
+                     " \\_           _/\n"
+                     "   \\_________/\n";
+
+  fstream MyFile;
+
+  MyFile.open("dogger.txt");
+
+  if (MyFile.is_open()) {
+    MyFile << dog << endl;
+    cout << "It Worked" << endl;
+    MyFile.close();
+  }
+
+  cout << dog << endl;
+
+  return 0;
+}
