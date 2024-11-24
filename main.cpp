a45612baa9e647d007d199a58d4ed04024c6fa9f
jt_gladden <jt_gladden@aol.com>
2024-11-23 18:40:53 -0700

    Bear moves across screen as expected

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
-#include <fstream>
+#include <chrono>
 #include <iostream>
-#include <iterator>
+#include <stdlib.h>
 #include <string>
+#include <thread>
 
 using namespace std;
 
 int main() {
-
-  const char dog[] = "      / \\__\n"
-                     "     (    @\\___\n"
-                     "     /         O\n"
-                     "    /   (_____/\n"
-                     "   /_____/   U\n"
-                     "  /           \\\n"
-                     " /             \\\n"
-                     "|               |\n"
-                     " \\_           _/\n"
-                     "   \\_________/\n";
-
-  cout << dog << endl;
+  const string dog[] = {"      / \\__\n",      "     (    @\\___\n",
+                        "     /         O\n",  "    /   (_____/\n",
+                        "   /_____/   U\n",    "  /           \\\n",
+                        " /             \\\n", "|               |\n",
+                        " \\_           _/\n", "   \\_________/\n"};
+  int size = sizeof(dog) / sizeof(dog[0]);
+  int count = 10;
+  for (int x = 0; x < count; x++) {
+    this_thread::sleep_until(chrono::system_clock::now() + chrono::seconds(1));
+    system("clear");
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < x; j++) {
+        cout << "\t";
+      }
+      cout << dog[i];
+    }
+  }
 
   return 0;
 }
