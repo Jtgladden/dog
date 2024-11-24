cb528199c4e98717b84797990c365ad4539e17e0
jt_gladden <jt_gladden@aol.com>
2024-11-23 17:34:03 -0700

    still broken

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <string>
 
 using namespace std;
@@ -16,22 +17,48 @@ int main() {
                      "|               |\n"
                      " \\_           _/\n"
                      "   \\_________/\n";
-
   fstream MyFile;
+  int count;
 
-  for (int i; i < 10; i++) {
+  cout << "How many Dog?" << endl;
+  cin >> count;
+
+  for (int i = 1; i < count; i++) {
 
     MyFile.open("dogger_" + to_string(i) + ".txt", fstream::out);
 
     // This prints the dog to a file
     if (MyFile.is_open()) {
       MyFile << dog << endl;
-      cout << "It Worked" << endl;
       MyFile.close();
     }
   }
+  MyFile.clear();
 
-  cout << dog << endl;
+  string line;
+  streampos savedPos = MyFile.tellp();
+
+  for (int i = 1; i < count; i++) {
+    cout << "Iteration: " << i << endl;
+    MyFile.open("dogger_" + to_string(i) + ".txt");
+
+    if (MyFile.is_open()) {
+      cout << "File opened" << endl;
+
+      savedPos = MyFile.tellp();
+      while (getline(MyFile, line)) {
+        MyFile.seekp(savedPos);
+        for (int j = 1; j < i; j++) {
+          MyFile << " ";
+          cout << "Space Printed for the " << j << "th time" << endl;
+        }
+        if (MyFile << line << endl)
+          cout << "line printed: " << line << endl;
+      }
+    }
+    MyFile.close();
+    MyFile.clear();
+  }
 
   return 0;
 }
